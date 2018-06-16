a5afaea7b995fe1fb64dcb43accaf20fe801781d
walkingscript <alexander.bondarenko2016@gmail.com>
2018-06-16 19:35:05 +0200

    Included library for executing tags from music files

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import sys
 
-def main():    
-    for item in sys.argv:
-        print(item)
+from mutagen.easyid3 import EasyID3
 
+def main():
+    for item in sys.argv:
+        song = EasyID3(item)
+        print(song)
     input()
 
 if __name__ == '__main__':
