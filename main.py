b8d52c0bc24660b3e08aaf74663304970434bd7c
walkingscript <alexander.bondarenko2016@gmail.com>
2018-06-17 20:28:14 +0200

    in the begining

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import os
 import sys
 
-from mutagen.easyid3 import EasyID3
-
 def main():
-    for item in sys.argv:
-        song = EasyID3(item)
-        print(song)
+    args = tuple(sys.argv)
     input()
 
 if __name__ == '__main__':
