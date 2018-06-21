dd18ff267c5143efe45f6e56fd838fb86f00b7cb
walkingscript <alexander.bondarenko2016@gmail.com>
2018-06-21 02:33:27 +0200

    Test config

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ class Settings():
     def __init__(self):
         self.start_set = {
             'Тест': {'work_dir': r'C:\Users\ocean\Desktop\DownloadManager\nn_rep\test_dir',
-                         'data_dir': r'C:\Users\ocean\Desktop\DownloadManager\nn_rep\data_dir'}
+                     'data_dir': r'C:\Users\ocean\Desktop\DownloadManager\nn_rep\data_dir'}
         }
