aef66559f6e78a86dcb5c3f3734ee33c40d67cdd
walkingscript <alexander.bondarenko2016@gmail.com>
2018-09-17 23:44:34 +0200

    Changes.

--- main.py
+++ main.py
@@ -61,38 +61,38 @@ class DownloadControl():
         file_count = len(self.files_in_work_dir)
         pb = progressbar.ProgressBar(min_value=0, max_value=file_count)
         while len(self.files_in_work_dir) > 0:
-            #try:
-            print(''.join((colorama.Fore.MAGENTA, colorama.Style.BRIGHT, "Группировка...")))
-            file = self.files_in_work_dir.pop()
-            devided = file.split('.')
-            file_name = ''.join(devided[0:-1])
-            file_extension = devided[-1]
-            path_for_file = self.extensions_path.get(file_extension.lower(), self.data_dir)
-            os.makedirs(path_for_file, exist_ok=True)
-            from_ = os.path.join(self.work_dir, file)
-            to = os.path.join(path_for_file, file)
+            try:
+                print(''.join((colorama.Fore.MAGENTA, colorama.Style.BRIGHT, "Группировка...")))
+                file = self.files_in_work_dir.pop()
+                devided = file.split('.')
+                file_name = ''.join(devided[0:-1])
+                file_extension = devided[-1]
+                path_for_file = self.extensions_path.get(file_extension.lower(), self.data_dir)
+                os.makedirs(path_for_file, exist_ok=True)
+                from_ = os.path.join(self.work_dir, file)
+                to = os.path.join(path_for_file, file)
 
-            # является ли файл zip-архивом, если да - разархивируем
-            if zipfile.is_zipfile(from_):
-                destination_zip = os.path.join(self.unpacked_zip_dir, file_name)
-                os.makedirs(destination_zip, exist_ok=True)
-                zf = zipfile.ZipFile(from_)
-                zf.extractall(destination_zip)
-                zf.close()
+                # является ли файл zip-архивом, если да - разархивируем
+                if zipfile.is_zipfile(from_):
+                    destination_zip = os.path.join(self.unpacked_zip_dir, file_name)
+                    os.makedirs(destination_zip, exist_ok=True)
+                    zf = zipfile.ZipFile(from_)
+                    zf.extractall(destination_zip)
+                    zf.close()
 
-            while os.path.exists(to):
-                new_file_name = ''.join((file_name, '-', str(uuid.uuid4()), '.', file_extension))
-                to = os.path.join(path_for_file, new_file_name)
+                while os.path.exists(to):
+                    new_file_name = ''.join((file_name, '-', str(uuid.uuid4()), '.', file_extension))
+                    to = os.path.join(path_for_file, new_file_name)
 
-            response = shutil.copy(from_, to)
-            if response == to:
-                os.remove(from_)
-            
-            # изменение цвета
-            print(colorama.Fore.CYAN, colorama.Style.BRIGHT)
-            pb.value += 1
-            #except Exception as e:
-            #    print(''.join((colorama.Fore.RED, colorama.Style.BRIGHT, "Вызвано исключение: ")), e.__class__)
+                response = shutil.copy(from_, to)
+                if response == to:
+                    os.remove(from_)
+                
+                # изменение цвета
+                print(colorama.Fore.CYAN, colorama.Style.BRIGHT)
+                pb.value += 1
+            except Exception as e:
+               print(''.join((colorama.Fore.RED, colorama.Style.BRIGHT, "Вызвано исключение: ")), e.__class__)
             
                 
 def main():
