6839b92e8c012e577d82f1abf509f19462ce2932
walkingscript <alexander.bondarenko2016@gmail.com>
2018-09-17 20:21:58 +0200

    main.py

--- settings.py
+++ settings.py
@@ -1,6 +1,15 @@
+import json
+
 class Settings():
-    def __init__(self):
-        self.start_set = {
-            'Тест': {'work_dir': r'C:\Users\ocean\Desktop\DownloadManager\nn_rep\test_dir',
-                     'data_dir': r'C:\Users\ocean\Desktop\DownloadManager\nn_rep\data_dir'}
-        }
+    def __init__(self, config_path):
+        self.load_settings(config_path)
+
+    def load_settings(self, path_to_config_file):
+        """Функция для загрузки пользовательских настроек."""
+        with open(path_to_config_file, 'r', encoding='utf-8') as json_config_file:
+            self.configurational_dict = dict(json.loads(json_config_file.read()))
+        
+
+if __name__ == "__main__":
+    input('''Это модуль настроек. Он не является главным.
+    Для запуска используйте main.py''')
