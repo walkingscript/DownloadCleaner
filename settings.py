b0ec3434e4ed02b9fac2bb12e6d2ffb10afba3fc
walkingscript <alexander.bondarenko2016@gmail.com>
2020-10-09 20:13:52 +0300

    Fixed errors in the design of code.

--- settings.py
+++ settings.py
@@ -1,13 +1,17 @@
 import json
 
+
 class Settings():
     def __init__(self, config_path):
         self.load_settings(config_path)
 
     def load_settings(self, path_to_config_file):
         """Функция для загрузки пользовательских настроек."""
-        with open(path_to_config_file, 'r', encoding='utf-8') as json_config_file:
-            self.configurational_dict = dict(json.loads(json_config_file.read()))
+        with open(path_to_config_file, 'r', encoding='utf-8')
+            as json_config_file:
+            self.configurational_dict = dict(json.loads(
+                json_config_file.read()
+            ))
         
 
 if __name__ == "__main__":
